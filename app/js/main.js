53dc8616fe59d2a6b157111054cbc179696a2ec9
HappySK <shravanyoserene1729@gmail.com>
2020-09-02 23:15:56 +0530

    Form Inputs have retreived successfully

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,18 @@
 $(document).ready(() => {
     $('form').submit(() => {
-        alert('Form Submitted')
+        var name = $('#emp-name').val()
+        var email = $('#emp-email').val()
+        var option = document.getElementById('Designation')
+        var desg = option.options[option.selectedIndex].value
+        var date = $('#emp-hire-date').val()
+        var salary = $('#emp-salary').val()
+        var emp = { 
+            name:name,
+            email:email,
+            desg:desg,
+            date:date,
+            salary:salary
+        }
+        console.log(JSON.stringify(emp))
     })
 })
