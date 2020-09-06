64dfd1bef774ef4de8d50cbb640e805617a44f85
HappySK <shravanyoserene1729@gmail.com>
2020-09-06 10:00:29 +0530

    REST API is created and consumed by a third party app

--- app/data.php
+++ app/data.php
@@ -1,20 +1,17 @@
 <?php
+    
+    // Using the REST API in order to consume the data
+    
     if(isset($_GET['action']) && !empty($_GET['action']))
     {
         $action = $_GET['action'];
         $url = "http://localhost/projects/EmpAPI/app/empDir_API.php?action=$action";
         $client = curl_init($url);
         curl_setopt($client,CURLOPT_RETURNTRANSFER,1);
-        $resource = curl_exec($client);
-        if(!empty($resource))
-        {
-            echo $resource;
-        }
-        else
-        {
-            echo "Empty Resources";
-        }
     }
+
+    // Using the REST API to Create an Employee from the third party APP
+
     else if(isset($_POST['emp'],$_POST['action']) && !empty($_POST['action']) && !empty($_POST['emp']))
     {
         $url = "http://localhost/projects/EmpAPI/app/empDir_API.php";
@@ -23,14 +20,17 @@
         curl_setopt($client,CURLOPT_POST,1);
         curl_setopt($client,CURLOPT_POSTFIELDS,"emp=$emp");
         curl_setopt($client,CURLOPT_RETURNTRANSFER,true);
-        $resource = curl_exec($client);
-        if(!empty($resource))
-        {
-            echo $resource;
-        }
-        else
-        {
-            echo "Empty Resources";
-        }
+    }
+
+    // Manipulation of responses
+
+    $resource = curl_exec($client);
+    if(!empty($resource))
+    {
+        echo $resource;
+    }
+    else
+    {
+        echo "Empty Resources";
     }
 ?>
